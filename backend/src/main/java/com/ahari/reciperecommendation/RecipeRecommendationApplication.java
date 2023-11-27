6b61048028223d1778aa4ca47993faf0f6ea60dc
ananaziz96 <ananaziz15@gmail.com>
2023-11-27 16:02:33 -0500

    first commit

---
+++ backend/src/main/java/com/ahari/reciperecommendation/RecipeRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.ahari.reciperecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeRecommendationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ahari/reciperecommendation/RecipeRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahari.reciperecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
